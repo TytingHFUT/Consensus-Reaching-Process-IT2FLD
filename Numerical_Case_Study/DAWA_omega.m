45298382fc9bbb828d954dd2eef9fcd98cad8ad1
田永廷 内蒙古工业大学 <106376373+TytingHFUT@users.noreply.github.com>
2025-06-30 10:12:47 +0800

    Delete Numerical_Case_Study directory

--- Numerical_Case_Study/DAWA_omega.m
+++
@@ -1,4 +0,0 @@
-function [M_c] = DAWA_omega(M_1,M_2,M_3,M_4,omega)
-%加权聚合
-M_c = M_1.*omega(1)+M_2.*omega(2)+M_3.*omega(3)+M_4.*omega(4);
-end
--- Numerical_Case_Study/def.m
+++
@@ -1,5 +0,0 @@
-function [def9s0] = def(IT2FN_i)
-%输入一个IT2FN-i对其进行去模糊，并输出去模糊结果
-temp =[IT2FN_i(1:2),IT2FN_i(4:5)];
-def9s0=((sum(temp)+(3+IT2FN_i(6))*IT2FN_i(3))/(3+IT2FN_i(6)))*0.5;
-end
